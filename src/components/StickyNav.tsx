116aab63770b57fa1edc56cade10f007a99ed364
ilanfridman23-eng <ilanfridman23@gmail.com>
2026-03-28 12:37:17 -0400

    Reduced font sizes in Footer and StickyNav components for consistency and improved design

--- src/components/StickyNav.tsx
+++ src/components/StickyNav.tsx
@@ -28,7 +28,7 @@ const StickyNav = () => {
           target="_blank"
           rel="noopener noreferrer"
           className="font-display font-bold tracking-tight text-white"
-          style={{ fontSize: 240 }}
+          style={{ fontSize: 20 }}
         >
           mabbly
         </a>
